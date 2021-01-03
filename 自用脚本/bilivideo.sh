93ae10f43db3980f73bcf037db17ae1c4a13f3fa
XGMO9 <941558072@qq.com>
2021-01-03 18:15:01 +0800

    支持自动识别并命名文件

--- 自用脚本/bilivideo.sh
+++ 自用脚本/bilivideo.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 for dir in $(ls -d */*)
 do
-  echo "$dir" && cd "$dir" && ffmpeg -i video.m4s -i audio.m4s -codec copy video.mp4 && cd .. && cd ..
+  cd "$dir" && name=`cat ../entry.json | jq -r '.title'` && count=`cat ../entry.json | jq -r '.ep.index'`
+  filename="$name"_"$count"
+  ffmpeg -i video.m4s -i audio.m4s -codec copy $filename.mp4 && cd .. && cd ..
 done
